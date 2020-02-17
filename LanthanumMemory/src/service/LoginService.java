cfa1f40fa625cc68c3dfc6ee61f27e0a6deb5324
Takafumi <takafumi.a304@gmail.com>
2020-02-17 13:12:27 +0900

    アカウントテーブルと入力情報の照合処理を追加

--- LanthanumMemory/src/service/LoginService.java
+++ LanthanumMemory/src/service/LoginService.java
@@ -1,19 +1,46 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import model.User;
 
 public class LoginService {
 
 	/**
-	 * ログインの認証処理
+	 * 入力されたログイン名とパスワードがDBに登録されているか判定を行う
+	 * true:ログイン成功
+	 * false:再入力
+	 * @throws ClassNotFoundException
 	 */
-	public boolean executeLogin(User user)
+	public boolean executeLogin(User loginUser) throws ClassNotFoundException
 	{
-		if(user.getUserName().equals("test") && user.getPass().equals("1234"))
+		String url = "jdbc:postgresql://localhost:5432/test";
+		String user = "takafumi";
+		String pass = "taka1224";
+
+		try{
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection(url, user, pass);
+			String sql = "SELECT NAME, PASSWORD FROM LM_ACCOUNTS;";
+			PreparedStatement pstm = conn.prepareStatement(sql);
+			ResultSet result = pstm.executeQuery();
+			while(result.next())
+			{
+				if(loginUser.getUserName().equals(result.getString("name")) && loginUser.getPass().equals(result.getString("password")))
+				{
+					return true;
+				}
+			}
+			return false;
+		}catch(SQLException e)
 		{
-			return true;
+			e.printStackTrace();
+			return false;
 		}
-		return false;
 	}
 
 }
